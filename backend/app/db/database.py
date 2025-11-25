d696ca8f465c1fc63efb4d70046280407a6b8bd7
Albert Chore <79362590+achore26@users.noreply.github.com>
2025-11-25 14:53:19 +0300

    Folder and DB integration

---
+++ backend/app/db/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+# Load .env
+env_path = os.path.join(os.path.dirname(__file__), "../../.env")
+load_dotenv(env_path)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
